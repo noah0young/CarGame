164e09b9eff3caeb2c56edb9c07863cf8987d1a4
noah0young <drnoahyoung@gmail.com>
2023-08-11 23:44:11 -0400

    Coyote time, quick jump, and started the trip mechanic. Though, we should consider how we want the trip mechanic to affect the player

--- CarGame/Assets/Scripts/CarPlayer.cs
+++ CarGame/Assets/Scripts/CarPlayer.cs
@@ -1,31 +1,63 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class CarPlayer : MonoBehaviour
 {
-    
     [Header("Basic Movement")]
     private Rigidbody2D myRigidbody;
+    protected float curAcceleration;
     [SerializeField] protected float acceleration = 1f;
+    protected float curDeceleration;
     [SerializeField] protected float deceleration = .5f;
+    protected float curMaxSpeed;
     [SerializeField] protected float maxSpeed = 10f;
-    [SerializeField] protected float jumpSpeed = 10f;
+    
     [Header("Basic Jumping")]
+    [SerializeField] protected float jumpSpeed = 10f;
+    [SerializeField] protected float minJumpSpeed = 2f;
     [SerializeField] private string jumpKey = "j";
     [SerializeField] private float jumpRaycastHeight = .6f;
     [SerializeField] private LayerMask floorMask;
+    private bool prevOnGround;
+    protected bool onGround { get; private set; }
+    private bool jumped = false;
+    [SerializeField] private float coyoteTime = .1f;
+    private float remainingCoyoteTime = 0;
+    [SerializeField] private float quickJumpTime = .1f;
+    private float quickJumpTimeRemaining = 0; // This will automatically jump when you next reach the ground
 
-    // Start is called before the first frame update
+    [Header("Tripping")]
+    [SerializeField] private float tripDecrease = .3f;
+    [SerializeField] private float tripTime = .5f;
+    private IEnumerator tripCoroutine;
+
     protected void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        curAcceleration = acceleration;
+        curDeceleration = deceleration;
+        curMaxSpeed = maxSpeed;
+        tripCoroutine = null;
     }
 
     protected void Update()
     {
+        ResetOnGround();
         TryJump();
+        UpdateRemainingJumpTimers();
+    }
+
+    private void UpdateRemainingJumpTimers()
+    {
+        if (remainingCoyoteTime > 0)
+        {
+            remainingCoyoteTime -= Time.deltaTime;
+        }
+        if (quickJumpTimeRemaining > 0)
+        {
+            quickJumpTimeRemaining -= Time.deltaTime;
+        }
     }
 
     protected void FixedUpdate()
@@ -33,21 +65,21 @@ public class CarPlayer : MonoBehaviour
         Move();
     }
 
-    protected void Move()
+    protected virtual void Move()
     {
         float direction = Input.GetAxis("Horizontal");
         Vector2 velocity = myRigidbody.velocity;
         if (direction != 0)
         {
             // Acceleration
-            velocity.x += direction * acceleration * Time.fixedDeltaTime;
-            if (velocity.x > maxSpeed)
+            velocity.x += direction * curAcceleration * Time.fixedDeltaTime;
+            if (velocity.x > curMaxSpeed)
             {
-                velocity.x = maxSpeed;
+                velocity.x = curMaxSpeed;
             }
-            else if (velocity.x < -maxSpeed)
+            else if (velocity.x < -curMaxSpeed)
             {
-                velocity.x = -maxSpeed;
+                velocity.x = -curMaxSpeed;
             }
         }
         else
@@ -55,7 +87,7 @@ public class CarPlayer : MonoBehaviour
             // Deceleration
             if (velocity.x > 0)
             {
-                velocity.x -= deceleration * Time.fixedDeltaTime;
+                velocity.x -= curDeceleration * Time.fixedDeltaTime;
                 if (velocity.x < 0)
                 {
                     velocity.x = 0;
@@ -63,7 +95,7 @@ public class CarPlayer : MonoBehaviour
             }
             if (velocity.x < 0)
             {
-                velocity.x += deceleration * Time.fixedDeltaTime;
+                velocity.x += curDeceleration * Time.fixedDeltaTime;
                 if (velocity.x > 0)
                 {
                     velocity.x = 0;
@@ -75,16 +107,89 @@ public class CarPlayer : MonoBehaviour
 
     protected void TryJump()
     {
-        if (CanJump() && Input.GetKeyDown(jumpKey))
+        if (CanJump() && ShouldJump())
         {
+            jumped = true;
             Vector2 velocity = myRigidbody.velocity;
             velocity.y = jumpSpeed;
             myRigidbody.velocity = velocity;
+            StartCoroutine(EndJump());
         }
     }
 
-    protected bool CanJump()
+    private IEnumerator EndJump()
     {
-        return Physics2D.Raycast(transform.position, Vector2.down, jumpRaycastHeight, floorMask);
+        yield return new WaitUntil(() => ReleasedJump());
+        // This lets the player end their jump early and have a shorter jump
+        if (myRigidbody.velocity.y > minJumpSpeed)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, minJumpSpeed);
+        }
+    }
+
+    protected virtual bool CanJump()
+    {
+        return onGround || remainingCoyoteTime > 0;
+    }
+
+    protected bool PressedJump()
+    {
+        return Input.GetKeyDown(jumpKey);
+    }
+
+    protected bool ShouldJump()
+    {
+        return PressedJump() || quickJumpTimeRemaining > 0;
+    }
+
+    protected bool ReleasedJump()
+    {
+        return !Input.GetKey(jumpKey);
+    }
+
+    protected void ResetOnGround()
+    {
+        prevOnGround = onGround;
+        onGround = Physics2D.Raycast(transform.position, Vector2.down, jumpRaycastHeight, floorMask);
+        if (onGround)
+        {
+            jumped = false;
+        }
+        else if (!jumped && !onGround && prevOnGround)
+        {
+            remainingCoyoteTime = coyoteTime;
+        }
+        else if (!onGround && PressedJump())
+        {
+            quickJumpTimeRemaining = quickJumpTime;
+        }
+    }
+
+    protected virtual IEnumerator Trip()
+    {
+        Debug.Log("Tripped");
+        curAcceleration = acceleration - tripDecrease;
+        curDeceleration = deceleration - tripDecrease;
+        curMaxSpeed = maxSpeed - tripDecrease;
+        yield return new WaitForSeconds(tripTime);
+        curAcceleration = acceleration;
+        curDeceleration = deceleration;
+        curMaxSpeed = maxSpeed;
+        tripCoroutine = null;
+        Debug.Log("Tripped End");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Trip"))
+        {
+            if (tripCoroutine != null)
+            {
+                StopCoroutine(tripCoroutine);
+                tripCoroutine = null;
+            }
+            tripCoroutine = Trip();
+            StartCoroutine(tripCoroutine);
+        }
     }
 }
